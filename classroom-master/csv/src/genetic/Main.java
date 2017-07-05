28d46ba368052a3bfac57e33b7d3f2323cc6b48e
lmasi <lmasi@naver.com>
2017-07-05 10:35:49 +0900

    tmp

--- classroom-master/csv/src/genetic/Main.java
+++ classroom-master/csv/src/genetic/Main.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.Collections;
 import java.util.Random;
 import java.util.Arrays;
+import java.io.*;
 
 import genetic.csv.*;
 import genetic.data.*;
@@ -26,14 +27,15 @@ public class Main
 		}
 	}
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		 BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
 		ClassSystem system = new ClassSystem(args[0]);
 		system.selectSemester(2013, 1);
 
 			Gene gene = ClassManager.getInstance().makeGene();
 
-			for(int i=1; i < 5; i++)
+			for(int i=1; i < 1000; i++)
 			{
 				GreedyAdaptor rA = new GreedyAdaptor();
 				rA.setGene(gene);
@@ -43,6 +45,12 @@ public class Main
 			System.out.println("====");
 
 		for(ClassGene _class : gene.getClassGene())
-			System.out.println(_class + "\t" + _class.getClassRoom().getName());
+		{
+			String str = (_class + "\t" + _class.getClassRoom().getName());
+			System.out.println(str);
+			out.write(str); out.newLine();
+		}
+
+		out.close();
 	}
 }
